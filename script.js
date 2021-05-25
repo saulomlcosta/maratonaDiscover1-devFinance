00b9a79a436e210b8e4d4cb72c85f4b8c352dc6c
Saulo Costa <saulomlcosta10@gmail.com>
2021-05-25 17:23:56 -0300

    Adding the transactions
    Using JS to add another transaction and format the currency to our locale.

--- script.js
+++ script.js
@@ -5,19 +5,25 @@ const transactionsData = [
   {
      id: 1,
      description: 'Luz',
-     amout: -50000,
+     amount: -50000,
      date: '23/01/2021'
   },
   {
     id: 2,
     description: 'Website',
-    amout: 500000,
+    amount: 500000,
     date: '23/01/2021'
   },
   {
     id: 3,
     description: 'Internet',
-    amout: -20000,
+    amount: -20000,
+    date: '23/01/2021'
+  },
+  {
+    id: 4,
+    description: 'App',
+    amount: 25000,
     date: '23/01/2021'
   },
 ]
@@ -34,19 +40,51 @@ const transaction = {
   }
 }
 
-const DOM = {
-  
-  innerHTMLtransaction() {
-    
-    const html = ` 
-    <tr>
-        <td class="description">Internet</td>
-        <td class="expense">-R$ 200,00</td>
-        <td class="date">23/01/2021</td>
-        <td>
-          <img src="./assets/minus.svg" alt="Remover transação">
-        </td>
-    </tr>
-    `
+const DOM = {  
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transactionsData, index) {      
+      const tr = document.createElement('tr')
+      tr.innerHTML = DOM.innerHTMLTransaction(transactionsData)
+
+      DOM.transactionsContainer.appendChild(tr)
+
+    },    
+    innerHTMLTransaction(transactionsData) {
+      const cssClass = transactionsData.amount > 0 ? "income" : "expense"
+
+      const amount = utils.formatCurrency(transactionsData.amount)
+
+      const html = `         
+            <td class="description">${transactionsData.description}</td>
+            <td class="${cssClass}">${amount}</td>
+            <td class="date">${transactionsData.date}</td>
+            <td>
+              <img src="./assets/minus.svg" alt="Remover transação">
+            </td>       
+        `
+
+        return html
+    }
+}
+
+const utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : ""
+
+    value = String(value).replace(/\D/g, "")
+
+    value = Number(value) / 100
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+
+    return signal + value  
   }
 }
+
+transactionsData.forEach(function(transactionsData) {
+  DOM.addTransaction(transactionsData)
+})
